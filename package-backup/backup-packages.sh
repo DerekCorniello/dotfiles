367deeb58e872b6cffe75b2c1355814c4c38b9ce
Derek Corniello <corniedj@mail.uc.edu>
2025-03-11 11:35:04 -0400

    updated backup script

--- package-backup/backup-packages.sh
+++ package-backup/backup-packages.sh
@@ -4,10 +4,8 @@
 mkdir -p ~/package-backup
 
 # grabs every package
-pacman -Qqe > ~/package-backup/pacman-packages.txt
-yay -Qqm > ~/package-backup/yay-packages.txt
-flatpak list --columns=application > ~/package-backup/flatpak-packages.txt
-
-pkill -SIGRTMIN+8 waybar
+pacman -Qqe > ~/dotfiles/package-backup/pacman-packages.txt
+yay -Qqm > ~/dotfiles/package-backup/yay-packages.txt
+flatpak list --columns=application > ~/dotfiles/package-backup/flatpak-packages.txt
 
 echo "Packages backed up successfully."
