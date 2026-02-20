ae862b6769c607e4a8f3ae59b1018bc3720b8d6b
Derek Corniello <corniedj@mail.uc.edu>
2026-02-19 19:48:50 -0500

    removed unused logic

--- nvim/.config/nvim/lua/core/autocmd.lua
+++ nvim/.config/nvim/lua/core/autocmd.lua
@@ -1,5 +1,4 @@
 -- Highlight on yank
-socal builtin = require('telescope.builtin')
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = "Highlight when yanking text"
