6e1fa8ad09e03cdb86359d1344c6d9d1a369927f
Derek Corniello <corniedj@mail.uc.edu>
2026-02-21 20:24:19 -0500

    fixed some intellij issues

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -6,8 +6,9 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- joins next line keeping cursor at one point
+-- joins lines keeping cursor at one point
 vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "K", "mzK`z")
 
 -- keeps cursor in middle of screen
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
