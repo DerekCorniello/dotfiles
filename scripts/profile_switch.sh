0934cf5a78c62fb4a611fe3e180a63cff91d0f3b
Derek Corniello <corniedj@mail.uc.edu>
2026-06-08 17:05:07 -0700

    notification fixes

--- scripts/profile_switch.sh
+++ scripts/profile_switch.sh
@@ -3,4 +3,4 @@
 asusctl profile next
 current_level=$(asusctl profile get | awk '/Active profile:/ {print $NF}')
 
-notify-send -t 2000 -h string:x-canonical-private-synchronous:active-profile -i application-default "Profile Switch" "Current Level: $current_level"
+notify-send "Profile Switch" -t 2000 -h string:x-canonical-private-synchronous:active-profile -i application-default "Current Level: $current_level"
